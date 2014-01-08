515a96ca0d898fe6ade144af9a7174a49e30f5fe
Randell Quitain <cprjk.buzz@gmail.com>
2014-01-08 17:28:31 +0800

    Updated Demo page.
    Signed-off-by: Randell Quitain <cprjk.buzz@gmail.com>

--- theme/scripts/stickish.js
+++ theme/scripts/stickish.js
@@ -1,68 +1,66 @@
 /*!
-* Instagram Sticky Header v.1
-* Randell Quitain [@cprjk]
-* Licensed under the MIT license
-*/
+ * Instagram Sticky Header v.1
+ * Randell Quitain [@cprjk]
+ * Licensed under the MIT license
+ */
+ 
+var stickish = {
+    init: function (options, elem) {
+        this.options = $.extend({}, this.options, options);
+        this.elem = elem;
+        this.$elem = $(elem);
+        this._start();
+        return this;
+    },
+    options: {},
+    _start: function () {
+        this.$elem.each(function (i) {
+            var currSticky = $(this).wrap('<div class="sticky-header" />');
+            currSticky.parent().height(currSticky.outerHeight());
+            $.data(currSticky[0], 'pos', currSticky.offset().top);
+        });
+    },
+    scrolling: function (element) {
+        $(element).each(function (i) {
+            var currSticky = $(this),
+                nextSticky = $(element).eq(i + 1),
+                prevSticky = $(element).eq(i - 1),
+                pos = $.data(currSticky[0], 'pos'),
+                nextPos, prevPos;
 
-;(function ( $, window, document, undefined ) {
+            if (pos <= $(window).scrollTop()) {
+                currSticky.addClass('fixed');
+                nextPos = $.data(nextSticky[0], 'pos') - currSticky.outerHeight();
+                if (nextSticky.length > 0 && currSticky.offset().top >= nextPos) {
+                    currSticky.addClass('absolute').css('top', nextPos);
+                }
+            } else {
+                currSticky.removeClass('fixed');
+                prevPos = $.data(currSticky[0], 'pos') - prevSticky.outerHeight();
+                if (prevSticky.length > 0 && $(window).scrollTop() <= prevPos) {
+                    prevSticky.removeClass('absolute').removeAttr('style');
+                }
+            }
+        });
 
-    var pluginName = 'stickish',
-    defaults = {
-        sticky: '.stickish-item'
-    };
-
-    function Plugin ( element, options ) {
-        this.element = element;
-        this.settings = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
     }
+};
 
-    Plugin.prototype = {
-        init: function () {
-            $(this.settings.sticky).each(function(){
-                var currSticky = $(this).wrap('<div class="sticky-header" />');
-                currSticky.parent().height(currSticky.outerHeight());
-                $.data(currSticky[0], 'pos', currSticky.offset().top);
-            });
-            this.scrolling(this.element, this.settings);
-        },
-        scrolling: function () {
-            var sticky = this.settings.sticky;
-            window.onscroll = function() {
-                $(sticky).each(function(i) {
-                    var currSticky = $(this)
-                    , nextSticky = $(sticky).eq(i+1)
-                    , prevSticky = $(sticky).eq(i-1)
-                    , pos = $.data(currSticky[0], 'pos')
-                    , nextPos
-                    , prevPos;
-
-                    if (pos <= $(window).scrollTop()) {
-                        currSticky.addClass('fixed');
-                        nextPos = $.data(nextSticky[0], 'pos') - currSticky.outerHeight()
-                        if (nextSticky.length > 0 && currSticky.offset().top >= nextPos) {
-                            currSticky.addClass('absolute').css('top', nextPos);
-                        }
-                    } else {
-                        currSticky.removeClass('fixed');
-                        prevPos = $.data(currSticky[0], 'pos')  - prevSticky.outerHeight();
-                        if (prevSticky.length > 0 && $(window).scrollTop() <= prevPos) {
-                            prevSticky.removeClass('absolute').removeAttr('style');
-                        }
-                    }
-                });
-            }; 
-        }
+if (typeof Object.create !== "function") {
+    Object.create = function (o) {
+        function F() {}
+        F.prototype = o;
+        return new F();
     };
+}
 
-    $.fn[ pluginName ] = function ( options ) {
-        return this.each(function() {
-            if ( !$.data( this, 'plugin_' + pluginName ) ) {
-                $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+$.plugin = function (name, object) {
+    $.fn[name] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, name)) {
+                $.data(this, name, Object.create(object).init(
+                    options, this));
             }
         });
     };
-
-})( jQuery, window, document );
+};
